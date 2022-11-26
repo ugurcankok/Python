83f2a0a42a5597f8bcfc7ff6ec99f62317afdb70
ugurcankok <ugurcankok0@gmail.com>
2022-11-26 17:58:58 +0300

    Add some example of list comprehension

--- src/List/ListComprehension.py
+++ src/List/ListComprehension.py
@@ -3,9 +3,9 @@
 # [expression if condition else result for item in list]
 # [(expression1, expression2) for item in list] for item in list]]
 
-list = [0,1,2,3,4,5]
+list = [0, 1, 2, 3, 4, 5]
 
 print([i for i in list])
-print([i*10 for i in list if i%2 == 0])
-print([i if i%2 == 0 else "odd number" for i in list])
-print([i*1.18 for i in list if i >0])
+print([i * 10 for i in list if i % 2 == 0])
+print([i if i % 2 == 0 else "odd number" for i in list])
+print([i * 1.18 for i in list if i > 0])
