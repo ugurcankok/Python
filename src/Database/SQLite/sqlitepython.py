0eb2c40617b67b0f04e0143304194c5d2ccf65e0
ugurcankok <ugurcankok0@gmail.com>
2023-01-07 12:57:11 +0300

    Reformat code and change function name

--- src/Database/SQLite/sqlitepython.py
+++ src/Database/SQLite/sqlitepython.py
@@ -4,38 +4,46 @@ con = sqlite3.connect("dersler.db")
 
 cursor = con.cursor()
 
-def tabloolustur():
+
+def createTable():
     cursor.execute("CREATE TABLE IF NOT EXISTS ogrenciler (ad TEXT, soyad TEXT, numara INT, ogrencinotu INT)")
     con.commit()
     con.close()
 
-def degerekle():
+
+def insertRecord():
     cursor.execute("INSERT INTO ogrenciler VALUES ('kerem', 'kok',9090,2342)")
-   # cursor.execute("INSERT INTO ogrenciler (ad,soyad,numara,ogrencinotu) VALUES (?,?,?,?)",(ad,soyad,numara,ogrencinotu))
+    # cursor.execute("INSERT INTO ogrenciler (ad,soyad,numara,ogrencinotu) VALUES (?,?,?,?)",(ad,soyad,numara,
+    # ogrencinotu))
     con.commit()
     con.close()
 
-def degergoster():
-    #cursor.execute("SELECT ad FROM ogrenciler WHERE soyad='Kok' AND numara=1234")
+
+def showRecord():
+    # cursor.execute("SELECT ad FROM ogrenciler WHERE soyad='Kok' AND numara=1234")
     cursor.execute("SELECT * FROM ogrenciler")
     data = cursor.fetchall()
     for i in data:
         print(i)
 
-def degerguncelle():
+
+def updateRecord():
     print("Ilk Değerler -------------------------")
-    degergoster()
+    showRecord()
 
     cursor.execute("UPDATE ogrenciler SET ad='ugur' WHERE numara=1234")
 
     print("Güncel Değerler ----------------------")
-    degergoster()
+    showRecord()
 
-def degersil():
+
+def deleteRecord():
     print("Ilk Değerler -------------------------")
-    degergoster()
+    showRecord()
     cursor.execute("DELETE FROM ogrenciler WHERE ogrencinotu=6789")
     print("Son Durum -------------------------")
-    degergoster()
+    showRecord()
 
-degersil()
+
+if __name__ == "__main__":
+    deleteRecord()
