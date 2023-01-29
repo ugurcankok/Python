10188d68400255bde2216690f2972321151fbbad
ugurcankok <ugurcankok0@gmail.com>
2023-01-29 20:41:07 +0300

    Remove punctuations

--- src/Database/PostgreSQL/postgresql.py
+++ src/Database/PostgreSQL/postgresql.py
@@ -8,11 +8,11 @@ db = psycopg2.connect(user = "username",
 
 post_cursor = db.cursor()
 
-create_db = """ CREATE TABLE names( id SERIAL PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL ); """
+create_db = """ CREATE TABLE names( id SERIAL PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL ) """
 
-insert_record = """ INSERT INTO names(first_name, last_name) VALUES ('ugur', 'kok'); """
+insert_record = """ INSERT INTO names(first_name, last_name) VALUES ('ugur', 'kok') """
 
-select_record = """ SELECT * FROM names; """
+select_record = """ SELECT * FROM names """
 
 post_cursor.execute(select_record)
 
@@ -21,7 +21,7 @@ list_records = post_cursor.fetchall()
 for list_record in list_records:
     print(list_record)
 
-update_record = """ UPDATE names SET first_name = 'ugurcan' where id = 1; """
+update_record = """ UPDATE names SET first_name = 'ugurcan' where id = 1 """
 
 delete_record = """ DELETE FROM names where first_name = 'ugur' """
 
